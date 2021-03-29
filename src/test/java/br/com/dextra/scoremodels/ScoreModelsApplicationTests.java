388f95859f38265025aa56f0e381363b2f43349b
gicesario <gisaa94@gmail.com>
2021-03-29 19:03:16 -0300

    versão 1

--- src/test/java/br/com/dextra/scoremodels/ScoreModelsApplicationTests.java
+++ src/test/java/br/com/dextra/scoremodels/ScoreModelsApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.dextra.imlookupto;
+package br.com.dextra.scoremodels;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ImLookUpToApplicationTests {
+class ScoremodelsApplicationTests {
 
 	@Test
 	void contextLoads() {
