47d7d2db2b6f950e4985154b5d95e7b8831b4ef1
gicesario <gisaa94@gmail.com>
2021-03-31 20:59:58 -0300

    tentativa 16

--- src/main/java/br/com/dextra/scoremodels/BootOnline.java
+++ src/main/java/br/com/dextra/scoremodels/BootOnline.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BootOnline {
+
 	@GetMapping("/")
 	  public String bootOnline() {
         return "Boot ta on";
