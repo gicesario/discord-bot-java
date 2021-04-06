4087baead6731656d5c62013165a587b0c4aa360
gicesario <gisaa94@gmail.com>
2021-04-06 16:43:28 -0300

    pegar todos os Member

--- src/main/java/br/com/dextra/scoremodels/event/ProcessaMensagem.java
+++ src/main/java/br/com/dextra/scoremodels/event/ProcessaMensagem.java
@@ -1,7 +1,13 @@
 package br.com.dextra.scoremodels.event;
 
+import java.math.BigInteger;
+import java.util.List;
+
 import br.com.dextra.scoremodels.utils.CommandosUtils;
+import discord4j.common.util.Snowflake;
+import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.Message;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class ProcessaMensagem {
@@ -9,7 +15,11 @@ public class ProcessaMensagem {
     public Mono<Void> processarComandoRecebido(Message mensagem) {
 
     	TipoComando cmd = CommandosUtils.getComando(mensagem.getContent().toUpperCase());
-    	//mensagem.getAuthor().stream().forEach(m -> System.out.println(m.getUsername()));
+    	Flux<Member> g = mensagem.getClient().getGuildMembers(Snowflake.of(new BigInteger("826179915346608221")));
+
+    	List<Member> list1 = g.collectSortedList().block();
+    	list1.forEach(System.out::println);
+
     	return Mono.just(mensagem)
 	  	          .filter(msg -> msg.getAuthor().map(user -> !user.isBot()).orElse(false))
 	  	          .filter(msg -> msg.getContent().toUpperCase().contains(cmd.name()))
